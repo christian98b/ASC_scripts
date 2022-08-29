e966471a4de821445f3b52137a93efb88337626c
district987 <45963946+district987@users.noreply.github.com>
2022-08-29 18:52:40 +0200

    Update backupdatabase.sh

--- backupdatabase.sh
+++ backupdatabase.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-date="$(date +'%d/%m/%Y')"
-mariabackup --backup \ --target-dir=/var/mariadb/backup/date \ --user=laravelapp_mitgliederverwaltunguser --password=mypassword
-echo("Backup erstellt")
+datum ="$(date +'%d-%m-%Y')"
+directory ="/var/mariadb/backup/$datum"
+lowerdir ="/var/mariadb/backup"
+password = "mypassword"
+user = "mydbuser"
+
+
+mariabackup --backup --target-dir=$directory --user=$user --password=$password
+echo "backup created"
+cd $lowerdir
+zip -r $directory.zip $datum
+echo "created zip"
+rm -r "${directory}"
+echo "delete old folder"
