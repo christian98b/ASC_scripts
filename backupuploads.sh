ed411def6107c8dd6efea5fb6c3c0cbdc3fa71ab
district987 <45963946+district987@users.noreply.github.com>
2022-10-14 16:01:56 +0200

    Create backupuploads.sh

---
+++ backupuploads.sh
@@ -0,0 +1,28 @@
+#folder to backup
+folderunder=/var/www/html/app_mitgliederverwaltung/storage/app
+folder=uploads
+#credentials to connect to ftp
+FTPUSER=
+FTPPW=
+FTPSERVER=
+#directory that will be created on ftp and the one that will be deleted
+TODAY=$(date +"%Y_%m_%d")
+RMDATE=$(date +"%Y_%m_%d" -d '7 days ago') 
+TMPDIR=/media/backups
+backup_file=$TMPDIR/backup.zip
+
+echo -n "Create Zip Archive"
+cd ${folderunder} 
+zip -r ${backup_file} ${folder}
+echo -n "Uploading files via FTP... "
+lftp << EOF
+open sftp://${FTPUSER}:${FTPPW}@${FTPSERVER}
+cd _UploadBackup/
+mkdir ${TODAY}
+cd ${TODAY}
+mput -E ${TMPDIR}/*
+cd ..
+rm -rf ${RMDATE}
+bye
+EOF
+echo "Done."
